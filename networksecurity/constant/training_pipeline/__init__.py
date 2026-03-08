6c7916011578dcdf61b66f970e418243b3a7e2f0
Priya raut <priyaraut20112000@gmail.com>
2026-03-08 23:20:02 +0530

    Data ingestion completed'

---
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -0,0 +1,30 @@
+import os 
+import sys 
+import numpy as np
+import pandas as pd
+
+"""
+defining common constant variable for training pipeline
+"""
+TARGET_COLUMN = "Result"
+PIPELINE_NAME: str = "NetworkSecurity"
+ARTIFACT_DIR: str = "Artifacts"       #all pipeline outputs store here
+FILE_NAME: str = "phisingData.csv"
+
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+
+SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")    #used to define--column types,required columns,validation rules
+
+SAVED_MODEL_DIR =os.path.join("saved_models")
+MODEL_FILE_NAME = "model.pkl"
+
+"""
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
+"""
+DATA_INGESTION_COLLECTION_NAME: str = "NetworkData"        ##mongodb collection name
+DATA_INGESTION_DATABASE_NAME: str = "PRIYAAI"              ##mongodb database name
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"            #this folder stores ingestion outputes(artifacts/data_ingestion)
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"    ##this will store row dataset copy(artifacts/data_ingestion/feature_store/phising_data.csv)
+DATA_INGESTION_INGESTED_DIR: str = "ingested"              ##this folder stores train and test data(artifacts/data_ingestion/ingested/train.csv,test.csv)
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2        ##split ratio
