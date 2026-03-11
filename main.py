ee6e87e9c756b87c3bac6aca83299b9838564d48
Priya raut <priyaraut20112000@gmail.com>
2026-03-11 19:55:10 +0530

    data validation completed

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from networksecurity.components.data_ingestion import DataIngestion
-
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataingestionConfig
+from networksecurity.entity.config_entity import DataingestionConfig,DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
 
@@ -10,11 +10,19 @@ if __name__=='__main__':
     try:
         trainingpipelineconfig=TrainingPipelineConfig()
         dataingestionconfig=DataingestionConfig(trainingpipelineconfig)
+        
         data_ingestion=DataIngestion(dataingestionconfig)
         logging.info("Initiate the data ingestion")
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
-
+        logging.info("data initiation completed")
         print(dataingestionartifact)
+        datavalidationconfig=DataValidationConfig(trainingpipelineconfig)
+        datavalidation=DataValidation(dataingestionartifact,datavalidationconfig)
+        logging.info("initiate the data validation")
+        data_validation_artifact=datavalidation.initiate_data_validation()
+        logging.info("data validation completed")
+        print(data_validation_artifact)
+        
 
     except Exception as e:
            raise NetworkSecurityException(e,sys)
--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -28,3 +28,14 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"            #this folder stores i
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"    ##this will store row dataset copy(artifacts/data_ingestion/feature_store/phising_data.csv)
 DATA_INGESTION_INGESTED_DIR: str = "ingested"              ##this folder stores train and test data(artifacts/data_ingestion/ingested/train.csv,test.csv)
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2        ##split ratio
+
+
+"""
+Data Validation related constant start with DATA_VALIDATION VAR NAME
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
