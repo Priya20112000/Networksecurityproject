73980d1860dd112a6da7dcbfda9892c28e365254
Priya raut <priyaraut20112000@gmail.com>
2026-03-13 17:01:40 +0530

    data transformtaion done

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataingestionConfig,DataValidationConfig
+from networksecurity.entity.config_entity import DataingestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
+
+from networksecurity.components.data_transformation import DataTransformation
 import sys
 
 if __name__=='__main__':
@@ -16,12 +18,26 @@ if __name__=='__main__':
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
         logging.info("data initiation completed")
         print(dataingestionartifact)
+
         datavalidationconfig=DataValidationConfig(trainingpipelineconfig)
         datavalidation=DataValidation(dataingestionartifact,datavalidationconfig)
         logging.info("initiate the data validation")
         data_validation_artifact=datavalidation.initiate_data_validation()
         logging.info("data validation completed")
         print(data_validation_artifact)
+
+        data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        logging.info("initiate the data transformation")
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        logging.info("data transformation completed")
+        print(data_transformation_artifact)
+
+
+
+
+
+
         
 
     except Exception as e:
